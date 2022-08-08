e590a825d3a4357d525d01e58e0482764189c3db
Thomas Cowart <thomas.cowart@duke.edu>
2022-08-08 12:58:03 -0400

    Adds arg parsing to expect script

--- jsom/__init__.py
+++ jsom/__init__.py
@@ -1,3 +1,20 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Connect to a Jupyter notebook server on SOM-HPC")
+
+    parser.add_argument("-a", "--address", help="address of ssh server", required=True)
+    parser.add_argument("-u", "--username", help="ssh username", required=True)
+    parser.add_argument("-k", "--key", help="private key file to use", required=True)
+    parser.add_argument("-c", "--conda", default="jupyter", help="conda environment containing jupyter server to activate", required=True)
+
+    return parser.parse_args()
+
+def expect(address, username, pkey_file, conda_env):
+    pass
 
 def run():
-    print("Hello World")
+    args = parse_args()
+
+    expect(args.address, args.username, args.key, args.conda)
