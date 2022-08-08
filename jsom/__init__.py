57ebe3b2614f701d01dc4a41ccf82e59f2c96cf6
Thomas Cowart <thomas.cowart@duke.edu>
2022-08-08 14:44:38 -0400

    SSH to somhpc, start jupyter, get further commands

--- jsom/__init__.py
+++ jsom/__init__.py
@@ -1,4 +1,8 @@
 import argparse
+import re
+import time
+
+from pexpect import pxssh
 
 
 def parse_args():
@@ -12,7 +16,41 @@ def parse_args():
     return parser.parse_args()
 
 def expect(address, username, pkey_file, conda_env):
-    pass
+    print(f"Connecting to {username}@{address}")
+    ssh = pxssh.pxssh(encoding="utf-8", options={
+        "StrictHostKeyChecking": "no"
+    })
+    ssh.login(server=address, username=username, ssh_key=pkey_file, sync_original_prompt=False)
+    print(f"Connected to {address}")
+    ssh.sendline(f"conda activate {conda_env}")
+    ssh.prompt()
+    print(f"Activated \"{conda_env}\" conda environment")
+    ssh.sendline("sbatch /data/shared/jobs/jupyter-notebook.job")
+    ssh.expect(re.compile(r"Submitted batch job (\d+)\r\n"))
+    job_id = ssh.match[1]
+    ssh.prompt()
+    print(f"Submitted job {job_id}")
+
+    time.sleep(2)
+
+    ssh.sendline(f"cat jupyter-notebook.job.{job_id}")
+    ssh.prompt()
+    job_info = ssh.before
+    run = re.search("srun.*", job_info)[0].trim()
+    hpcTunnel = re.search("ssh -NR.*", job_info)[0].trim()
+    localTunnel = re.search("ssh -NL.*")[0].trim()
+    jupyterPassword = re.search("password: (.*)")[1].trim()
+
+
+    ##
+    ## Clean Up
+    ##
+    ssh.sendline(f"scancel -f {job_id}")
+    ssh.prompt()
+    print(f"Cancelled jupyter notebook job {job_id}")
+    ssh.sendline(f"rm jupyter-notebook.job.{job_id}")
+    ssh.prompt()
+    print(f"Deleted log from jupyter notebook job {job_id}")
 
 def run():
     args = parse_args()
